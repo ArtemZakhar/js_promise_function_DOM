57679a2390d91b22fa9309b7fe34d135fb01d906
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-05 22:03:00 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,27 @@
+"use strict";
+const divTag = document.createElement("div");
+document.body.append(divTag);
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        element.addEventListener(eventName, ()=>{
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`);
+        });
+    });
+}
+const printMessage = (message)=>{
+    if (!divTag.classList.contains("message")) divTag.classList.add("message");
+    divTag.textContent = message;
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
